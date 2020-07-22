fa3e9570c536906f26f92d12e29f0025ba5c045d
Mike <mike@Mikes-iMac.local>
2020-07-21 20:56:29 -0500

    Initial Xcode Project Creation

---
+++ cwcYoutubeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cwcYoutubeApp
+//
+//  Created by Mike on 7/21/20.
+//  Copyright © 2020 Mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
